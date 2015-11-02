d55bf46b9d163d1ebe65daca944e35ce35cc8ac7
1314785 <nader.ghazoli@gmail.com>
2015-11-02 15:53:42 +0000

    The tree house page Having the blarg as google

--- helloSelector.php
+++ helloSelector.php
@@ -1,11 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The is the about us page</title>
+</head>
+<body>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: 1314785
- * Date: 02/11/2015
- * Time: 15:42
- */
 echo "<p>Would you like to say hello?</p>";
 echo "<a hef = helloselector.php?planet=earth/>Earth</a>";
 echo "<a hef = helloselector.php?planet=mars/>Mars</a>";
+?>
+</body>
+</html>
 
